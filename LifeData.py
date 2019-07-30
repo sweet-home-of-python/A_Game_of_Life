ed37b65f5a350957e6a2fe98a9ade74d1a23dd2c
Yaroslav <Yaroslav@DESKTOP-JCJQBR6>
2019-07-30 18:37:29 +0300

    fix

--- LifeData.py
+++ LifeData.py
@@ -85,7 +85,7 @@ class Person():
         dl2 = len(name2)
         sym2 = dl2 // 2
         name1 = name1[:sym1]
-        name2 = name2[:sym2]
+        name2 = name2[sym2:]
         name2 = name2.lower()
         print(name1+name2)
 
