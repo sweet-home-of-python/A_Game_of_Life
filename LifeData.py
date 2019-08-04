ba85e0567c86c835b2725015fde6c21270152331
Yaroslav <Yaroslav@DESKTOP-JCJQBR6>
2019-08-04 21:25:13 +0300

    изменения структуры

--- LifeData.py
+++ LifeData.py
@@ -78,17 +78,6 @@ class Person():
 
         Objects.get_object(self)
     
-    def new_name(name1,name2):
-        ''' создает новое имя из двух''' 
-        dl1 = len(name1)
-        sym1 = dl1 // 2
-        dl2 = len(name2)
-        sym2 = dl2 // 2
-        name1 = name1[:sym1]
-        name2 = name2[sym2:]
-        name2 = name2.lower()
-        print(name1+name2)
-
     def random_gender(self):
         genders = ['male','female']
         return rand.choice(genders)
---
+++ miscLife.py
@@ -0,0 +1,10 @@
+def new_name(name1,name2):
+    ''' создает новое имя из двух''' 
+    dl1 = len(name1)
+    sym1 = dl1 // 2
+    dl2 = len(name2)
+    sym2 = dl2 // 2
+    name1 = name1[:sym1]
+    name2 = name2[sym2:]
+    name2 = name2.lower()
+    return n_name
