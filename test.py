6fa1655381e12e73ccd4791abe9db58882c20541
Ярослав <@DESKTOP-9DD6V4F>
2019-08-05 11:13:15 +0300

    yar_note

--- test.py
+++ test.py
@@ -31,12 +31,28 @@ def new_name(name1,name2):
     return result
 
 
+ch_m = []
+ch_f = []
 
+print('Первое поколение\n')
 for i in range(0,20):
     mn = rand.choice(m_names)
     fn = rand.choice(f_names)
-    cn = new_name(fn,mn)
+    ch_m.append(new_name(fn,mn))
+    ch_f.append(new_name(mn,fn))
 
-    #print('{} + {} = {}'.format(mn,fn,cn))
 
-    print('{} {} {}'.format(cn,(rand.choice(podlezg)).lower(),(rand.choice(sush)).lower()))
+    print('{} {}ович'.format(ch_m[i],mn))
+    print('{} {}овна'.format(ch_f[i],mn))
+
+print('\nВторое поколение\n')
+for i in range(0,20):
+    mn = rand.choice(ch_m)
+    fn = rand.choice(ch_f)
+    nch_m = new_name(fn,mn)
+    nch_f = new_name(mn,fn)
+
+    print('{} {}ович'.format(nch_m,mn))
+    print('{} {}овна'.format(nch_f,mn))
+
+    #print('{} {} {}'.format(cn,(rand.choice(podlezg)).lower(),(rand.choice(sush)).lower()))
