5a02866c152f3eafdde815536d88a66831fb10f4
aid219 <aid219@DESKTOP-H9BSPJU>
2019-08-06 17:28:26 +0300

    перезлив

--- main.py
+++ main.py
@@ -21,18 +21,11 @@ pygame.display.set_caption('A Game of Life')
 #celsc = pygame.display.set_mode(game.resolution, display=0)
 #clock = pygame.time.Clock()
 
+persData = cells
 
 
-pers = Person("jake")# пиздюки
-pers2 = Person("fin")
-pers3 = Person("jake")
-pers4 = Person("fin")
-pers5 = Person("jake")
-pers6 = Person("fin")
-pers7 = Person("jake")
-pers8 = Person("fin")
-pers9 = Person("jake")
-pers10 = Person("fin")
+pers = Person("jake", persData)# пиздюки
+
 
 
 Play = True # Запуск
@@ -48,6 +41,7 @@ print(Objects.objects)
 
 while Play:
     sc.fill(game.colors['white'])# Заливка
+
     cell_visualisator(sc,cells.cells,2) # Работает медленно, нужно отображение переделывать/ Курю документацию
  
     drawer.drawObjects(Objects.objects,sc)
